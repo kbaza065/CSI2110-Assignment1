37e81cf30fd30fb5dc86842708878d3883fce256
Bazza <bazarjani.kian@gmail.com>
2020-10-25 21:57:31 -0400

    Added more comments to explain code

--- src/main/java/RankingPair.java
+++ src/main/java/RankingPair.java
@@ -13,9 +13,15 @@ public class RankingPair {
     private final int employerRanking;
     private final int studentRanking;
 
+    /**
+     * @param pair String containing a ranking pair of integers in the format "employer_ranking, student_ranking"
+     */
     public RankingPair(String pair) {
+        // Split the ranking to store each integer as a String in an array
         String[] rankings = pair.split(",");
+        // Parse String array at first index to retrieve employer_ranking
         this.employerRanking = Integer.parseInt(rankings[0]);
+        // Parse String array at second index to retrieve student_ranking
         this.studentRanking = Integer.parseInt(rankings[1]);
 
     }
